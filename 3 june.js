e144ac4575e52dabd47c85664d9b307aff2443bb
Rickshitha <rickshithaanandakumar@gmail.com>
2025-06-05 21:03:18 +0530

    Add files via upload

---
+++ 3 june.js
@@ -0,0 +1,6 @@
+function light_ON(){
+	document.getElementById('myimage').src='https://www.bulbamerica.com/cdn/shop/products/s14987_19f55a87-3f41-4a37-a0c6-3b010b274d4b.jpg?v=1649453376';
+	}
+	function light_OFF(){
+	document.getElementById('myimage').src='https://m.media-amazon.com/images/I/51IQ23SYLyL._AC_UF1000,1000_QL80_.jpg';
+	}
